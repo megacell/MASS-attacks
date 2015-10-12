d452c49851b5970d74c991ebde9638cccef71549
Jerome <jerome.thai.87@gmail.com>
2015-10-11 21:53:10 -0700

    start implementing optimal attack solver

---
+++ optimal_attack_solver.py
@@ -0,0 +1,16 @@
+'''
+Implement the solver for the optimal attack problem
+The problem is solved via block-coordinated descent
+using a combination of three algorithms:
+    - min_attack_solver in which the availabilities 'a' are fixed
+    - attack_routing_solver in which the 
+'''
+
+import numpy as numpy
+
+__author__ = 'jeromethai'
+
+
+class OptimalAttackSolver:
+    # class for the optimal attack solver
+    pass
