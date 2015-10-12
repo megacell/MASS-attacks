Fixed bugs in adjacency calculation

MAS_network.py
@@ -161,13 +161,13 @@ class Network:
         # Returns an adjacency matrix if we allow raduis r number of steps
         assert type(r) == int and r > 0, 'Incorrect r'
         if r == 1:
-            return np.copy(self.full_adjacency)
+            return np.copy(self.adjacency)
         else:
             # Since the graph is undirected, if we can reach node i in at most r
             # steps, we can reach it in either r-1 steps (odd length path from
             # origin to i) or r steps (even length path from origin to i)
-            r_steps = np.linalg.matrix_power(self.full_adjacency, r)
-            r_minus_1_steps = np.linalg.matrix_power(self.full_adjacency, r - 1)
+            r_steps = np.linalg.matrix_power(self.adjacency, r)
+            r_minus_1_steps = np.linalg.matrix_power(self.adjacency, r - 1)
             res = r_steps + r_minus_1_steps
             norm_adjacencies(res)
             return res

run_experiments.py
@@ -9,19 +9,17 @@ def cal_logo_experiment(adj):
 
     bal_rates, bal_routing = nw.balance()
     nw.combine()
-    nw.adjacency = nw.get_adjacencies(adj)
-    att_rates, att_routing = nw.min_attack(target, full_adj=False)
 
-    nw = load_network('data/queueing_params.mat')
-    bal_rates, bal_routing = nw.balance()
-    nw.combine()
-    att_rates_full, att_routing_full = nw.min_attack(target, full_adj=True)
+    res = []
+    for i in adj:
+        nw.full_adjacency = nw.get_adjacencies(i)
+        att_rates, att_routing = nw.min_attack(target, full_adj=True)
+        res.append(int(np.sum(att_rates)))
 
     print 'Passenger Arrival Rate:', np.sum(nw.rates)
     print 'Balance Cost: ', np.sum(bal_rates)
-    print 'Attack After Balance Cost (adjacency {}): {}'.format(adj, np.sum(att_rates))
-    print 'Attack After Balance Cost (full adjacency): ', np.sum(att_rates_full)
-
+    print 'Attack After Balance Cost (adjacency {}): {}'.format(adj, res)
+    return res
 
 if __name__ == '__main__':
-    cal_logo_experiment(1)
+    cal_logo_experiment(range(1, 10))