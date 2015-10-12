89bb7923ddfb2a722b877cddcd35b2760ed84d06
Jerome <jerome.thai.87@gmail.com>
2015-10-12 02:54:41 -0700

    add test_optimal_attack_solver.py

---
+++ tests/test_optimal_attack_solver.py
@@ -0,0 +1,27 @@
+'''
+Tests for optimal attack solver
+'''
+
+import unittest
+import numpy as np
+import MAS_network as MAS
+from optimal_attack_solver import OptimalAttackSolver
+
+
+__author__ = 'jeromethai'
+
+
+class TestOptimalAttackSolver(unittest.TestCase):
+
+    def test_optimal_attack_solver_full_network(self):
+        network = MAS.load_network('data/queueing_params.mat')
+        network.budget = 1500.
+        k = np.where(network.new_availabilities() - 1. == 0.0)[0][0]
+        network.balance(cplex=True)
+        print 'total customer rate', np.sum(network.rates)
+        print 'total rebalancing rate', np.sum(network.attack_rates)
+        network.combine()
+        print 'min availability', np.min(network.availabilities())
+        print 'combined customer and rebalancing rates', np.sum(network.rates)
+        oas = OptimalAttackSolver(network, cplex=True, k=k)
+        # oas.solve(alpha=10., beta=1., max_iters_attack_rate=5)
