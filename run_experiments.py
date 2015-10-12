b7bc01bcde1f05df74757d5ba1cf9fdc485b558b
Chenyang Yuan <yuanchenyang@gmail.com>
2015-10-12 16:16:50 -0700

    Fixed bug

--- run_experiments.py
+++ run_experiments.py
@@ -3,13 +3,13 @@ from MAS_network import load_network
 from logo_to_availabilities import get_availabilities
 from pdb import set_trace as T
 
-def cal_logo_experiment():
+def cal_logo_experiment(adj):
     nw = load_network('data/queueing_params.mat')
     target = get_availabilities(nw.station_names)
 
     bal_rates, bal_routing = nw.balance()
     nw.combine()
-    nw.adjacencies = nw.full_adjacency #nw.get_adjacencies(100)
+    nw.adjacency = nw.get_adjacencies(adj)
     att_rates, att_routing = nw.min_attack(target, full_adj=False)
 
     nw = load_network('data/queueing_params.mat')
@@ -19,9 +19,9 @@ def cal_logo_experiment():
 
     print 'Passenger Arrival Rate:', np.sum(nw.rates)
     print 'Balance Cost: ', np.sum(bal_rates)
-    print 'Attack After Balance Cost (full adjacency 1): ', np.sum(att_rates)
-    print 'Attack After Balance Cost (full adjacency 2): ', np.sum(att_rates_full)
-    print 'Last 2 should be the same'
+    print 'Attack After Balance Cost (adjacency {}): {}'.format(adj, np.sum(att_rates))
+    print 'Attack After Balance Cost (full adjacency): ', np.sum(att_rates_full)
+
 
 if __name__ == '__main__':
-    cal_logo_experiment()
+    cal_logo_experiment(1)
